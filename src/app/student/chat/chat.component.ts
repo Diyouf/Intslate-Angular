860503429e0f4e007538442410873e588db0f7d1
Diyouf <diyoufkv7@gmail.com>
2023-08-16 15:51:37 +0530

    Error message in Chat

--- src/app/student/chat/chat.component.ts
+++ src/app/student/chat/chat.component.ts
@@ -1,37 +1,47 @@
-import { Component, OnInit, ViewChild , AfterViewChecked ,ElementRef} from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  AfterViewChecked,
+  ElementRef,
+} from '@angular/core';
 import { Observable, tap } from 'rxjs';
 import { ChatService } from './chat.service';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Message, allChat, returnData, teacherData } from './chat.interface';
-
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+  styleUrls: ['./chat.component.css'],
 })
-export class ChatComponent implements OnInit ,AfterViewChecked{
-  constructor(private chatService : ChatService,private fb:FormBuilder){}
-  newMessage$!:Observable<string>
-  messages:string [] =[]
-  teacherData!:teacherData[]
-  readonly studentId:string | null = localStorage.getItem('studentId')
-  connectionData !:returnData
-  allMessages:allChat[] = []
+export class ChatComponent implements OnInit, AfterViewChecked {
+  constructor(
+    private chatService: ChatService,
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar
+  ) {}
+  newMessage$!: Observable<string>;
+  messages: string[] = [];
+  teacherData!: teacherData[];
+  readonly studentId: string | null = localStorage.getItem('studentId');
+  connectionData!: returnData;
+  allMessages: allChat[] = [];
   @ViewChild('chatContainer') private chatContainer!: ElementRef;
-
+  activeItemId: string | null = null;
 
   ngOnInit() {
     this.chatService.getNewMessage().subscribe(() => {
       this.loadMessages();
     });
 
-    this.loadAllTeacher()
+    this.loadAllTeacher();
   }
-  
+
   formData = this.fb.group({
-    message:'',
-  })
+    message: [''],
+  });
 
   ngAfterViewChecked() {
     this.scrollToBottom();
@@ -39,42 +49,56 @@ export class ChatComponent implements OnInit ,AfterViewChecked{
 
   scrollToBottom(): void {
     try {
-      this.chatContainer.nativeElement.scrollTop = this.chatContainer.nativeElement.scrollHeight;
-    } catch (err) { }
+      this.chatContainer.nativeElement.scrollTop =
+        this.chatContainer.nativeElement.scrollHeight;
+    } catch (err) {}
   }
 
-
-
-  onSubmit(){
-    const {message} = this.formData.value
-    if(!message){
-      return
-    }else{
-      const data:Message = {
-        senderName:<string>this.studentId,
-        message:message,
-        connectionId:<string>this.connectionData._id,
-        to:<{_id:string}>this.connectionData.connection?.teacher
+  onSubmit() {
+    const { message } = this.formData.value;
+    if (message && message.trim() && this.formData.value) {
+      if (!message) {
+        return;
+      } else {
+        const data: Message = {
+          senderName: <string>this.studentId,
+          message: message,
+          connectionId: <string>this.connectionData._id,
+          to: <{ _id: string }>this.connectionData.connection?.teacher,
+        };
+        this.chatService.sendMessage(data);
+        this.formData.reset();
       }
-      this.chatService.sendMessage(data)
-      this.formData.reset()
+    } else {
+      this.snackBar.open('Input field is empty', 'close', {
+        panelClass: 'custom-snackbar',
+        duration: 3000,
+      });
     }
   }
 
-  loadAllTeacher(){
-    this.chatService.loadAllTeacher().subscribe((res)=>{
-      this.teacherData = res
-    })
+  loadAllTeacher() {
+    this.chatService.loadAllTeacher().subscribe((res) => {
+      this.teacherData = res;
+    });
   }
 
+  addSuggestionToMessage(suggestion: string) {
+    const messageControl = this.formData.get('message');
+    if (messageControl) {
+      messageControl.setValue(suggestion);
+    }
+  }
 
   setConnection(teacherId: string | null) {
+    this.activeItemId = teacherId;
     const data = {
       teacherId: teacherId,
       studentId: this.studentId,
     };
-  
-    this.chatService.setConection(data)
+
+    this.chatService
+      .setConection(data)
       .pipe(
         tap((res) => {
           this.connectionData = res;
@@ -84,16 +108,12 @@ export class ChatComponent implements OnInit ,AfterViewChecked{
         this.loadMessages();
       });
   }
-  
-  loadMessages(){
-    this.chatService.loadMessages(this.connectionData._id).subscribe((res:allChat[])=>{
-      this.allMessages = res
-      
-      
-      
-    })
+
+  loadMessages() {
+    this.chatService
+      .loadMessages(this.connectionData._id)
+      .subscribe((res: allChat[]) => {
+        this.allMessages = res;
+      });
   }
-
-  
-
 }
--- src/app/teacher/chat/chat.component.ts
+++ src/app/teacher/chat/chat.component.ts
@@ -1,38 +1,39 @@
-import { Component, OnInit ,AfterViewChecked,ElementRef,ViewChild} from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewChecked,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 import { ChatService } from './chat.service';
-import { allChat, connectionData, studentData } from './chat.interface';
-import { FormBuilder } from '@angular/forms';
-import { tap } from 'rxjs';
+import { allChat, connectionData } from './chat.interface';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css'],
 })
-export class ChatComponent implements OnInit,AfterViewChecked {
-  constructor(private chatService: ChatService, private fb: FormBuilder) {}
+export class ChatComponent implements OnInit, AfterViewChecked {
+  constructor(
+    private chatService: ChatService,
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar
+  ) {}
   readonly id = localStorage.getItem('teacherId');
   @ViewChild('chatContainer') private chatContainer!: ElementRef;
   studentdata!: connectionData[];
   allchats: allChat[] = [];
   connection!: string | undefined;
-  studentId!: {id:string};
-  messages: string[] = [];
+  studentId!: { _id: string };
+  activeStudentId: string | null = null;
 
   ngOnInit(): void {
     this.loadConnection();
-    this.chatService
-      .getNewMessage()
-      .pipe(
-        tap((message: string) => {
-          this.messages.push(message);
-        })
-      )
-      .subscribe(() => {
-        this.allmessages();
-      });
-      
-      
+    this.chatService.getNewMessage().subscribe(() => {
+      this.allmessages();
+    });
   }
   ngAfterViewChecked() {
     this.scrollToBottom();
@@ -40,45 +41,42 @@ export class ChatComponent implements OnInit,AfterViewChecked {
 
   scrollToBottom(): void {
     try {
-      this.chatContainer.nativeElement.scrollTop = this.chatContainer.nativeElement.scrollHeight;
-    } catch (err) { }
+      this.chatContainer.nativeElement.scrollTop =
+        this.chatContainer.nativeElement.scrollHeight;
+    } catch (err) {}
   }
 
   loadConnection() {
     this.chatService.loadConnection(this.id).subscribe((res) => {
       this.studentdata = res;
-      console.log(this.studentdata);
     });
   }
 
-  loadChats(id: string | undefined, studentId:string ) {
-
-    console.log(studentId);
+  loadChats(id: string | undefined, studentId: string) {
+    this.activeStudentId = studentId;
 
     this.connection = id;
-    this.studentId ={
-      id : studentId
-    } ;
+    this.studentId = {
+      _id: studentId,
+    };
     this.allmessages();
   }
 
   allmessages() {
-    this.chatService.loadAllChats(this.connection).subscribe((res:allChat[]) => {
-      
-      this.allchats = res;
-      
-    });
+    this.chatService
+      .loadAllChats(this.connection)
+      .subscribe((res: allChat[]) => {
+        this.allchats = res;
+      });
   }
 
   formData = this.fb.group({
-    message: '',
+    message: ['', Validators.pattern('^[s\t\n]*$')],
   });
 
   onSubmit() {
     const { message } = this.formData.value;
-    if (!message) {
-      return;
-    } else {
+    if (message && message.trim() && this.formData.value) {
       const data = {
         senderName: this.id,
         message: message,
@@ -87,6 +85,11 @@ export class ChatComponent implements OnInit,AfterViewChecked {
       };
       this.chatService.sendMessage(data);
       this.formData.reset();
+    } else {
+      this.snackBar.open('Input field is empty', 'close', {
+        panelClass: 'custom-snackbar',
+        duration: 3000,
+      });
     }
   }
 }
